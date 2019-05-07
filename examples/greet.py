b9a9118537b1357f04f0a8b95a0f197903214ac8
wilfredinni <carlos.w.montecinos@gmail.com>
2019-05-07 14:56:41 -0400

    tox, coverage, examples, update dependencies

---
+++ examples/greet.py
@@ -0,0 +1,29 @@
+"""
+Very basic example that show how to build the simples CLI.
+"""
+from mary import Master, Command
+
+
+class Main(Master):
+    """
+    Sample CLI app for the Rose framework.
+    """
+
+
+class Greet(Command):
+    """
+    Greets someone
+    """
+
+    command_name = "greet"
+    argument = {"name": "Who do you want to greet?"}
+
+    def handler(self):
+        print(f"hello {self.argument}")
+
+
+app = Main()
+app.register([Greet])
+
+if __name__ == "__main__":
+    app.run()
